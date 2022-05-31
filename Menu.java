6d98dc7137f9d4750304afb66cc72299ba16b7a1
PedroCIglesias <101953888+PedroCIglesias@users.noreply.github.com>
2022-05-31 19:03:35 -0300

    Add files via upload

--- Menu.java
+++ Menu.java
@@ -2,15 +2,21 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.*;
+import java.util.regex.*;
+
+import javax.lang.model.util.ElementScanner6;
+
 import java.util.ArrayList;
 
 public class Menu {
     private ArrayList<Medicamento> medicamentos = new ArrayList<>();
 
     public Menu(){
-        leCSV();
+        leCSV2();
     }
 
     private void leCSV() {
@@ -54,6 +60,60 @@ public class Menu {
           e.printStackTrace();
         }
     }
+
+
+
+    private void leCSV2() {
+      String line = "";
+      String splitBy = ";";
+      try {
+        BufferedReader br = new BufferedReader(new FileReader("D:/Irmandade/Medicamentos/bin/TA_PRECO_MEDICAMENTO.csv"));
+        while ((line = br.readLine()) != null)
+        {
+          converter(line);
+        }
+        br.close();
+      }
+      catch(IOException e) {
+        e.printStackTrace();
+      }
+  }
+
+  public static List<String> split(String str){
+    return Stream.of(str.split("[;]"))
+      .map (elem -> new String(elem))
+      .collect(Collectors.toList());
+  }
+
+  public static List<Object> toObjectList(List<String> list){
+          boolean aspasAbertas =false;
+          List<String> lista = new ArrayList<String>();
+          List<Object> listaFinal = new ArrayList<Object>();
+          for(String s: list){
+            if(s.contains("\"")){
+              s.replace('"',' ');
+              if(aspasAbertas){
+                aspasAbertas=false;
+                lista.add(s);
+                listaFinal.add(lista);
+                lista.clear();
+              }
+              else aspasAbertas=true;
+            }
+            if(aspasAbertas){
+              lista.add(s);
+            }else
+            listaFinal.add(s);
+          }
+          return listaFinal;
+  }
+  public static Map<Colunas,Object> converter(String str){
+    List<Object> list = toObjectList((split(str)));
+    Map<Colunas, Object> map = new HashMap<>();
+    IntStream.range(0,list.size())
+      .forEach(index -> map.put(Colunas.values()[index],list.get(index)));
+    return map;
+  }
 }
 
 
